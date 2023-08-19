9e440273667bab996680b8388628fef7410311b2
thomas anderson da silva farias <98232934+thomasfarias95@users.noreply.github.com>
2023-08-19 13:31:54 -0300

    Update contaTerminal.java

--- src/contaTerminal.java
+++ src/contaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String name = scanner.next();
+
+        System.out.println("Seu numero da conta: ");
+        int number = scanner.nextInt();
+
+        System.out.println("Qual o numero da sua Agencia? : ");
+        String agency = scanner.next();
+
+        System.out.println("Seu saldo é de: ");
+        float balance = scanner.nextFloat();
+
+        System.out.printf(
+                "Olá, Sr." + name + ", sua conta: " + number + ", agencia: "
+                        + agency + ", com  saldo: " + balance + ", já está disponivel para saque");
     }
 }
+
